671bade7ec5752ad90881772497b4508715f2561
Vicerr <victorodoi741@gmail.com>
2025-05-23 21:26:34 +0100

    test logo 3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+window.addEventListener("load", () => {
+    const loader = document.getElementById("loader")
+    if (loader) loader.style.display = "none"
+})
 document.addEventListener("DOMContentLoaded", () => {
     const observer = new IntersectionObserver(
         (entries, obs) => {
